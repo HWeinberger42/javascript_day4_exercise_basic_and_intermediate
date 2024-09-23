5e5157992b548c73220514cfd2f6ecade18fbce9
Hermann Weinberger <miol@protonmail.com>
2024-09-23 14:52:07 +0200

    exercise 1point1

---
+++ script.js
@@ -0,0 +1,52 @@
+// exercise 1.1
+
+// // function printfullname(e){
+// // e.preventDefault()
+// // console.log("test");
+
+// // }
+// // document.querySelector("#submitOne").addEventListener("click", function() {
+    
+// //     let fname = document.querySelector("#firstName").value;
+
+    
+// //     document.getElementById("resultFormOne").innerHTML = `
+// //         <ul>
+// //             <li>First Name: ${fname}</li>
+// //             <li>Last Name: </li>
+// //             <li>Age: </li>
+// //         </ul>
+// //         `;
+// // })
+
+// // document.querySelector("#submitOne").addEventListener("click", printfullname)
+
+// function fillResultFormOne(wholeForm) {
+//     wholeForm.preventDefault();     // !!!<form> reloads page by default after submitting!!!
+
+//     let divResultFormOne = document.getElementById("resultFormOne");
+//     let fname = document.querySelector("#firstName").value;
+//     let lname = document.querySelector("#lastName").value;
+//     let a = document.querySelector("#age").value;
+
+   
+
+//     divResultFormOne.innerHTML = `
+//         <ul>
+//             <li id="firstFirstLi">First Name: ${fname}</li>
+//             <li>Last Name: ${lname}</li>
+//             <li>Age: ${a}</li>
+//         </ul>
+//     `;
+
+//     if (fname.length > 5) {
+//         document.getElementById("firstFirstLi").setAttribute("color", "green");
+//     } else {
+//         document.getElementById("firstFirstLi").style.color = "red";
+//     }
+// }
+
+// document.querySelector("#submitOne").addEventListener("click", fillResultFormOne)
+
+// exercise 2.1
+
